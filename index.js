f98e2f2d0dcd123e0ae4a2afa5f92b2558814d83
Atif Waheed <atif.waheed.cowlar@gmail.com>
2023-09-12 19:46:10 +0500

    basic server with basic home page

--- index.js
+++ index.js
@@ -4,9 +4,24 @@
 // const person1 = new Person("Atif Waheed", 23);
 // person1.greeting();
 
-const Logger = require("./logger");
-const logger = new Logger();
+// const Logger = require("./logger");
+// const logger = new Logger();
 
-logger.on("message", (data) => console.log("Called Listener: ", data));
-logger.log("Hello World");
-logger.log("Hello World 2");
+// logger.on("message", (data) => console.log("Called Listener: ", data));
+// logger.log("Hello World");
+// logger.log("Hello World 2");
+
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("<h1>Home Page</h1>");
+  }
+  console.log(req.url);
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server Running on ${PORT}...`));
