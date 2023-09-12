0deb24627f248185d73d8427f86c098234bb5be0
Atif Waheed <atif.waheed.cowlar@gmail.com>
2023-09-12 18:26:37 +0500

    file system, reference, path

---
+++ index.js
@@ -0,0 +1,6 @@
+const Person = require("./person");
+
+// console.log("Hello " + person.name +  " from Node.js");
+// console.log(person);
+const person1 = new Person("Atif Waheed", 23);
+person1.greeting();
